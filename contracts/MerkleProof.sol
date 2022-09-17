f4429042d19c5c25d6155860ff0fc443a85fb0f6
Laam Wong <laamwong3@gmail.com>
2022-09-17 08:39:56 +0800

    merkle proof nfts

---
+++ contracts/MerkleProof.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.7;
+
+contract MerkleProof {
+    function verify(
+        bytes32[] memory _proof,
+        bytes32 _root,
+        bytes32 _leaf,
+        uint _index
+    ) public pure returns (bool) {}
+}
