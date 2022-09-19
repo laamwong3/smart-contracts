fa0cfcd2e4432b660a19e583b4f9c86a9e8a6289
Laam Wong <laamwong3@gmail.com>
2022-09-19 10:05:07 +0800

    done first test

--- test/MetaTransaction.ts
+++ test/MetaTransaction.ts
@@ -1,4 +1,6 @@
-import { parseEther } from "ethers/lib/utils";
+import { expect } from "chai";
+import { BigNumber } from "ethers";
+import { arrayify, formatEther, parseEther } from "ethers/lib/utils";
 import { ethers } from "hardhat";
 
 describe("Meta Transaction Test", () => {
@@ -14,7 +16,50 @@ describe("Meta Transaction Test", () => {
     const [_, userAddress, relayerAddress, recipientAddress] =
       await ethers.getSigners();
 
-    const mintAmount = parseEther("10");
-    console.log(mintAmount);
+    const mintAmount = parseEther("10000");
+    // console.log(mintAmount);
+    const userTokenInstance = mockToken.connect(userAddress);
+    const mintTx = await userTokenInstance.mint(mintAmount);
+    await mintTx.wait(1);
+
+    const approveTx = await userTokenInstance.approve(
+      relayer.address,
+      BigNumber.from(
+        "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+      )
+    );
+    await approveTx.wait(1);
+
+    const transferAmount = parseEther("10");
+    const messageHash = await relayer.getHash(
+      userAddress.address,
+      transferAmount,
+      recipientAddress.address,
+      mockToken.address
+    );
+    // console.log(messageHash);
+    const signature = await userAddress.signMessage(arrayify(messageHash));
+    // console.log(signature);
+
+    const replayContractInstance = relayer.connect(relayerAddress);
+    const metaTx = await replayContractInstance.transfer(
+      userAddress.address,
+      transferAmount,
+      recipientAddress.address,
+      mockToken.address,
+      signature
+    );
+    await metaTx.wait(1);
+
+    const userBalance = await mockToken.balanceOf(userAddress.address);
+    const recipientBalance = await mockToken.balanceOf(
+      recipientAddress.address
+    );
+
+    // console.log(formatEther(userBalance));
+    // console.log(formatEther(recipientBalance));
+
+    expect(userBalance).lt(mintAmount);
+    expect(recipientBalance).gt(BigNumber.from(0));
   });
 });
