b31fdf741070a3634c2ab4178716c041accf30bd
Laam Wong <laamwong3@gmail.com>
2022-10-18 06:51:46 +0800

    create donation contract

--- contracts/Donation.sol
+++ contracts/Donation.sol
@@ -1,2 +1,18 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Donation is Ownable {
+    event Deposited(uint amount);
+    event Withdrawn(uint amount);
+
+    receive() external payable {
+        emit Deposited(msg.value);
+    }
+
+    function withdraw() external onlyOwner {
+        emit Withdrawn(address(this).balance);
+        selfdestruct(payable(msg.sender));
+    }
+}
